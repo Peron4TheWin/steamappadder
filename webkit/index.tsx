2e703395caa855cd975d35e77bd630f84f83f2ba
Juan Domingo Peron <frandonna12@gmail.com>
2025-08-22 22:05:00 -0300

    Added check if you cant add the app

--- webkit/index.tsx
+++ webkit/index.tsx
@@ -4,21 +4,15 @@ import { callable } from '@steambrew/webkit';
 const receiveFrontendMethod = callable<[{ message: string; status: boolean; count: number }], boolean>('Backend.receive_frontend_message');
 
 export default async function WebkitMain() {
-		// check URL
 		if (!location.href.startsWith("https://store.steampowered.com/app/")) return;
 
-		// avoid duplicates
-		//if (document.getElementById("add-app-to-library-btn")) return;
-
-		// create button
 		const btn = document.createElement("button");
 		btn.id = "add-app-to-library-btn";
 		btn.textContent = "Add app to library";
 		btn.style.cssText = `
 		cursor:pointer; border:none; border-radius:8px; padding:10px 14px;
 		font-weight:600; font-size:14px; line-height:1;
-		background:#1999ff; color:#fff; box-shadow:0 2px 6px rgba(0,0,0,.25);
-	  `;
+		background:#1999ff; color:#fff; box-shadow:0 2px 6px rgba(0,0,0,.25);`;
 
 		// click handler
 		btn.addEventListener("click", async () => {
@@ -26,11 +20,8 @@ export default async function WebkitMain() {
 				const success = await receiveFrontendMethod({
 					message: window.location.href
 				});
-				console.log(success);
-				console.log(success.message);
-				console.log(success.count);
-				console.log(success.status);
-				alert("Successfully added the app");
+				if (success) alert("Successfully added the app");
+				else alert("Failed to add app");
 			} catch (err) {
 				alert(err.response);
 			}
