1f50b3e0a381e987e011c191a5caaf90a3f95402
Juan Domingo Peron <94873456+Peron4TheWin@users.noreply.github.com>
2025-10-12 21:56:40 +0000

    removed old api, replaced with custom one (from 10s to 100ms of response time)

--- backend/main.py
+++ backend/main.py
@@ -11,36 +11,14 @@ def getSteamPath() -> str:
     #Need to check if that first one really returns the correct path
     #(winreg.QueryValueEx(winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Valve\Steam"), "SteamPath")[0])
 
-def download_and_extract(url, lua_folder, manifest_folder) -> bool:
+def download_and_extract(appid, lua_folder) -> bool:
     try:
-        disk = (str(os.environ["SYSTEMDRIVE"]))
-        temp_folder = rf"{disk}\temp\steam_mods"
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                        "AppleWebKit/537.36 (KHTML, like Gecko) "
-                        "Chrome/138.0.0.0 Safari/537.36",
-            "Accept": "application/json, text/plain, */*",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9"
-        }
-
-        response = requests.get(url, headers=headers, stream=True)
+        response = requests.get(f"http://api.perondepot.xyz/get_manifest?appid={appid}", stream=True)
         response.raise_for_status()  # error if download failed
-        with zipfile.ZipFile(BytesIO(response.content)) as z:
-            z.extractall(temp_folder)
-        for root, _, files in os.walk(temp_folder):
-            for file in files:
-                src_path = os.path.join(root, file)
+        file_name = os.path.join(lua_folder, f"{appid}.lua")  # or generate a name dynamically
+        with open(file_name, "w", encoding="utf-8") as f:
+            f.write(response.text)
 
-                if file.endswith(".lua"):
-                    dest_path = os.path.join(lua_folder, file)
-                elif "manifest" in file.lower():
-                    dest_path = os.path.join(manifest_folder, file)
-                else:
-                    continue
-                os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-                shutil.move(src_path, dest_path)
-        shutil.rmtree(temp_folder)
         return True
     except Exception as e:
         logger.log(e)
@@ -89,10 +67,7 @@ class Backend:
         if not m:
             return False
         luas=os.path.join(getSteamPath(),"config","stplug-in")
-        manifests = os.path.join(getSteamPath(), "config","manifests")
-        url = str(f"https://mellyiscoolaf.pythonanywhere.com/{int(m.group(1))}")
-        logger.log(url)
-        return download_and_extract(url,luas,manifests)
+        return download_and_extract(int(m.group(1)),luas)
     
 
 
